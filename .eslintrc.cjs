a589213847476656f24eaa53e7d7bddc53c92a30
邓智尹 <18682759294@163.com>
2023-01-16 17:11:40 +0800

    feat(code): add husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+/*
+ * @Author: Dzy
+ * @Date: 2023-01-16 16:33:37
+ * @LastEditors: Dzy
+ * @LastEditTime: 2023-01-16 16:45:58
+ * @Description: eslint文件配置
+ */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    // prettier 必须放在最后面
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+}
