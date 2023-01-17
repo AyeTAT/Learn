2af2060b6353ccc089dc0394ec51a39009618e17
邓智尹 <18682759294@163.com>
2023-01-17 13:18:36 +0800

    配置各类文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: Dzy
+ * @Date: 2023-01-16 15:20:50
+ * @LastEditors: Dzy
+ * @LastEditTime: 2023-01-17 13:17:23
+ * @Description: vite配置
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
